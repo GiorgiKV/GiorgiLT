3ad69cbf1ee4ce12316b15c1ec30288842977a01
GiorgiKV <giorgi.kvizhinadze@otago.ac.nz>
2017-07-12 10:31:10 +1200

    Changes made in drop_down_menus and server as GD recommended. App is working now

--- server.R
+++ server.R
@@ -18,15 +18,15 @@ shinyServer(function (input,output){
   tab_tot_dat <- callModule(league_data_select, "uncer_select", 
                             dat = tot_dat, main_char_str = main_char_str)
   
-  plot_tar_dat <- callModule(plot_data_select, dat = tar_dat, 
+  plot_tar_dat <- callModule(plot_data_select,"null", dat = tar_dat, 
                              main_char_str = main_char_str, plot_outcome = plot_outcome)
-  plot_tot_dat <- callModule(plot_data_select, dat = tot_dat, 
+  plot_tot_dat <- callModule(plot_data_select, "null", dat = tot_dat, 
                              main_char_str = main_char_str, plot_outcome = plot_outcome)
   
-  plot_height <- callModule(plot_height_server,  plot_dat = plot_tar_dat)
+  plot_height <- callModule(plot_height_server, "null", plot_dat = plot_tar_dat)
   
-  plot_tar_nest <- callModule(plot_nest_server, plot_dat = plot_tar_dat, plot_outcome = plot_outcome) 
-  plot_tot_nest <- callModule(plot_nest_server, plot_dat = plot_tot_dat, plot_outcome = plot_outcome) 
+  plot_tar_nest <- callModule(plot_nest_server,"null", plot_dat = plot_tar_dat, plot_outcome = plot_outcome) 
+  plot_tot_nest <- callModule(plot_nest_server, "null",plot_dat = plot_tot_dat, plot_outcome = plot_outcome) 
   
   callModule(plot_tar_render, "plot_tar", plot_height = plot_height,
              dat = plot_tar_nest)
@@ -35,8 +35,8 @@ shinyServer(function (input,output){
   
   callModule(plot_scatter_server, "plot_cep", plot_dat = plot_tar_dat)
   
-  tar_league_table <- callModule(table_server, dat = tab_tar_dat, uncer_ind = uncer_ind)
-  tot_league_table <- callModule(table_server, dat = tab_tot_dat, uncer_ind = uncer_ind)
+  tar_league_table <- callModule(table_server,"null", dat = tab_tar_dat, uncer_ind = uncer_ind)
+  tot_league_table <- callModule(table_server,"null", dat = tab_tot_dat, uncer_ind = uncer_ind)
   
   callModule(table_render, "table_tar", tar_league_table)
   callModule(table_render, "table_tot", tot_league_table)
